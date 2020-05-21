9dad3ca4de9cbf6a012bb9e3095b951fdef1d042
Jorge Nova <jorge.a.nova@gmail.com>
2020-05-21 14:42:14 -0500

    Proyecto con toda su funcionalidad

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import getData from '../utils/getData';
 const Home = async() => { 
     const characters = await getData();
     const view = `
-        <div class="characters">
+        <div class="Characters">
             ${characters.results.map(character => `
-                <article class="character-item">
+                <article class="Character-item">
                     <a href="#/${character.id}/">
                         <img src="${character.image}" alt="${character.name}">
                         <h2>${character.name}</h2>
